e8393b0f5f8775ce9a09e2dd5fc8b98cf7c7b065
Arsher-Elena <lenashmigol02@gmail.com>
2023-09-26 15:59:38 +0300

    hw6

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,10 @@
+import ContactForm from './Form';
+import ContactList from './ContactList';
+import Filter from './Filter';
+import { Container, PhoneBook, Title } from '../components/App.styled';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 export const App = () => {
   return (
     <div
@@ -6,11 +13,18 @@ export const App = () => {
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        fontSize: 36,
+        color: '#010101',
       }}
     >
-      React homework template
+      <Container>
+        <PhoneBook>Phonebook</PhoneBook>
+        <ContactForm />
+        <Title>Contacts</Title>
+        <Filter />
+        <ContactList />
+        <ToastContainer autoClose={2500} limit={3} />
+      </Container>
     </div>
   );
 };
